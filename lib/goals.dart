5e157be1e386d428323a775830ed0958cd7b8e5c
Jason Lee <jasonepic0801@gmail.com>
2019-04-06 22:33:03 +0800

    Goals backend
    Connected goals to the database and set down a data structure.

--- lib/goals.dart
+++ lib/goals.dart
@@ -28,6 +28,18 @@ FloatingActionButton goalsFloatingActionButton(BuildContext context) {
     child: Icon(Icons.library_add),
     elevation: 2.0,
     onPressed: () {
+      Goal goal = new Goal(
+          name: 'jason',
+          desc: 'hello jason',
+          timeSpent: new Duration(days: 1, hours: 2, minutes: 3, seconds: 4),
+          color: Colors.blue
+      );
+      Firestore.instance.collection('goals').document(user.uid).collection('userGoals').add({
+        'name': goal.name,
+        'desc': goal.desc,
+        'timeSpent': goal.timeSpent.toString(),
+        'colorValue': goal.color.value,
+      });
       print("Add goal!"); //TODO: Implement adding goals & archiving goals
     },
   );
@@ -37,14 +49,36 @@ class Goals extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        stream: Firestore.instance.collection('goals').document(user.uid).snapshots(),
+        stream: Firestore.instance.collection('goals').document(user.uid).collection('userGoals').snapshots(),
         builder: (context, snapshot) {
-          switch (currentLayout) {
-            case Layout.list:
-              return ListView.builder(itemBuilder: null); //TODO: Implement ListView goals
-            case Layout.grid:
-              return GridView.builder(gridDelegate: null, itemBuilder: null) //TODO: Implement GridView goals
-          }
+          print(Colors.blue.value);
+          if (snapshot.hasData) {
+            List goalList = [];
+            goalList = snapshot.data.documents.map((doc) => new Goal(
+              name: doc['name'],
+              desc: doc['desc'],
+              color: Color(doc['colorValue']),
+              timeSpent: parseDuration(doc['timeSpent']),
+            )).toList();
+            if (goalList.length != 0) {
+              switch (currentLayout) {
+                case Layout.list:
+                  return ListView.builder(
+                      itemCount: goalList.length,
+                      itemBuilder: (context, i) {
+                        return GoalsCard(goal: goalList[i],);
+                      }); //TODO: Implement ListView goals
+                case Layout.grid:
+                  return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200.0),
+                      itemBuilder: (context, i) {
+                        return GoalsCard(goal: goalList[i],);
+                      }
+                  ); //TODO: Implement GridView goals
+              }
+            } else return Center(child: Text('No goals. Add new goal?'),);
+          } else return Center(child: CircularProgressIndicator(),);
+
         }
     );
   }
@@ -65,12 +99,36 @@ class Goal {
 }
 
 class GoalsCard extends StatelessWidget {
+  final Goal goal;
+  GoalsCard({@required this.goal});
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Container(
         height: 100.0,
+        color: goal.color,
+        child: ListTile(
+          leading: Icon(Icons.library_music),
+          title: Text(goal.name),
+          subtitle: Text('${goal.desc}, Time spent: ${goal.timeSpent}'),
+        ),
       ),
     );
   }
+}
+
+Duration parseDuration(String s) {
+  int hours = 0;
+  int minutes = 0;
+  int micros;
+  List<String> parts = s.split(':');
+  if (parts.length > 2) {
+    hours = int.parse(parts[parts.length - 3]);
+  }
+  if (parts.length > 1) {
+    minutes = int.parse(parts[parts.length - 2]);
+  }
+  micros = (double.parse(parts[parts.length - 1]) * 1000000).round();
+  return Duration(hours: hours, minutes: minutes, microseconds: micros);
 }
