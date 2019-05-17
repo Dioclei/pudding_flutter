2fa1acd3245d14d2bc374e9a50b686b2acd9f9ca
HollowGale <48714991+HollowGale@users.noreply.github.com>
2019-05-17 15:34:20 +0800

    Styling Add Event Button in timetable.dart
    Aesthetics

--- lib/timetable.dart
+++ lib/timetable.dart
@@ -24,7 +24,21 @@ class Timetable extends StatelessWidget {
           ),
           RaisedButton(
             onPressed: (){},
-            child: const Text('+ Add Event'),
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Color(0xFF0D47A1),
+                      Color(0xFF1976D2),
+                      Color(0xFF42A5F5),
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('+ Add Event')
+            ),
           )
         ],
       ),
