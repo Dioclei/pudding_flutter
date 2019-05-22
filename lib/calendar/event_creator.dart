d43448452972857ee80fc1e2daf70d4789f9286a
HollowGale <48714991+HollowGale@users.noreply.github.com>
2019-05-22 18:48:10 +0800

    Created a page for adding events

---
+++ lib/calendar/event_creator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EventCreator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(30.0),
+          padding: const EdgeInsets.all(20.0),
+          color: Colors.white,
+          child: ListView(
+            children: <Widget>[
+              /*Event Creator Placeholder*/
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Event name'
+                ),
+              ),
+              /*Start Date and Time placeholder*/
+              Row(
+                mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+              ),
+              /*End Date and Time placeholder*/
+              Row(
+                mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
