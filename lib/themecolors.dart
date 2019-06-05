b50a4a7cd2a6e4ef5aa58bd27b8261f88427e369
Jason Lee <jasonepic0801@gmail.com>
2019-06-05 18:00:00 +0800

    Added a DialogTheme
    Easier to standardise dialogs

--- lib/themecolors.dart
+++ lib/themecolors.dart
@@ -4,6 +4,9 @@ final ThemeData themeData = ThemeData(
   fontFamily: 'Product Sans',
   primarySwatch: Colors.brown,
   canvasColor: Colors.transparent,
+  dialogTheme: DialogTheme(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+  ),
 );
 
 final backgroundColor = Colors.yellow[100];
