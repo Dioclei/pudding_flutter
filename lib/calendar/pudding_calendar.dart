29f5137970c82e56379d9dfbc1da245c28a009c7
Jason Lee <jasonepic0801@gmail.com>
2019-06-08 14:47:55 +0800

    Update timetable.dart
    Removed the appbar & floating action button. These will be in pudding_calendar.dart (the main calendar page)

--- lib/calendar/pudding_calendar.dart
+++ lib/calendar/pudding_calendar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'calendar_carousel.dart';
-
+import 'package:pudding_flutter/dateparser.dart';
+import 'package:pudding_flutter/calendar/timetable.dart';
 
 FloatingActionButton calendarFloatingActionButton(BuildContext context) {
   return FloatingActionButton(
@@ -13,276 +14,78 @@ FloatingActionButton calendarFloatingActionButton(BuildContext context) {
 }
 
 class PudCalendar extends StatefulWidget {
-
   @override
   _PudCalendarState createState() => _PudCalendarState();
 }
 
-class _PudCalendarState extends State<PudCalendar> with SingleTickerProviderStateMixin {
-
-  TabController _tabController;
-
-  List<Tab> dateTabs = [
-    Tab(
-      child: Text(
-        'MON\n3 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'TUE\n4 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'WED\n5 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'THU\n6 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'FRI\n7 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'SAT\n8 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'SUN\n9 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'MON\n10 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'TUE\n11 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-    Tab(
-      child: Text(
-        'WED\n12 Jun',
-        textAlign: TextAlign.center,
-      ),
-    ),
-  ];
+class _PudCalendarState extends State<PudCalendar> {
+  DateTime today;
+  DateTime currentDate;
+  PageController _pageController;
+  MediaQueryData queryData;
 
   @override
   void initState() {
-    _tabController = TabController(vsync: this, length: dateTabs.length, initialIndex: 3);
-    _tabController.addListener(_updateDateList);
+    today = DateTime.now();
+    currentDate = getDate(today);
+    _pageController = PageController(initialPage: 5000);
     super.initState();
-  }
-
-  void _updateDateList() {
-    setState(() {
-      int offset = _tabController.index - _tabController.previousIndex;
-      if (offset > 0) { //if user scrolled to the right
-        for (var i = offset; i > 0; i--) {
-          // TODO: Use a pageview instead.
-          // TODO: grab last date and add [offset] more dates, then delete the first [offset] dates actually dont need to delete?
-          dateTabs.add(Tab(
-            child: Text('TEST\n11 Aug', textAlign: TextAlign.center,),
-          ));
-          _tabController = TabController(vsync: this, length: dateTabs.length, initialIndex: 3);
-          _tabController.addListener(_updateDateList);
-        }
-      } else if (offset < 0) { //if user scrolled to the left
-        for (var i = offset; i < 0; i++) {
-          // TODO: grab first date and add [offset] more dates in the other direction (left), then delete the last [offset] dates
-          dateTabs.insert(0, Tab(
-            child: Text('TEST\n11 Aug', textAlign: TextAlign.center,)
-          ));
-          _tabController = TabController(vsync: this, length: dateTabs.length, initialIndex: 3);
-          _tabController.addListener(_updateDateList);
-        }
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    MediaQueryData queryData;
     queryData = MediaQuery.of(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calendar'),
+        title: Text(getMonthDay(currentDate)),
+        elevation: 0,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.event_note), onPressed: () {print('Pressed');})
+          IconButton(
+              icon: Icon(Icons.event_note),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: NewCalendar(),
+                      );
+                    });
+              })
         ],
-        bottom: TabBar(
-          isScrollable: true,
-          controller: _tabController,
-          tabs: dateTabs,
-        ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-          children: dateTabs.map((Tab tab) {
-            return Center(child:
-              SingleChildScrollView(
-                child: Table(
-                  columnWidths: {
-                    0: FixedColumnWidth((queryData.size.width)/3),
-                    1: FixedColumnWidth((queryData.size.width)/3*2)
-                  },
-                  border: TableBorder(horizontalInside: BorderSide(width: 0.5, color: Colors.grey), verticalInside: BorderSide(width: 0.5, color: Colors.grey)),
-                  children: <TableRow>[
-                    TableRow(
-                      children: [
-                        Container(
-                          height: 50,
-                        ),
-                        Container(
-                          height: 50,
-                        )
-                      ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            color: Colors.lightBlueAccent,
-                            height: 50,
-                          ),
-                          Container(
-                            height: 50,
-                          )
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-                    TableRow(
-                        children: [
-                          Container(
-                            height: 50,
-                          ),
-                          Container()
-                        ]
-                    ),
-
-                  ],
-
-                ),
-              ),
-            );
-          }).toList(),
+      body: Column(
+        children: <Widget>[
+          Container( /// Fake App Bar Extension
+            width: queryData.size.width,
+            color: Colors.brown,
+            child: NewCalendar(),
+          ),
+          Expanded(
+            child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (i) {
+                  int offset = i - 5000;
+                  setState(() {
+                    if (offset == 0) {
+                      currentDate = today;
+                    } else if (offset > 0) {
+                      currentDate =
+                          getDate(today.add(Duration(days: offset.abs())));
+                    } else {
+                      currentDate =
+                          getDate(today.subtract(Duration(days: offset.abs())));
+                    }
+                  });
+                },
+                itemBuilder: (context, i) {
+                  return Timetable(
+                    tooday: currentDate,
+                  );
+                }),
+          ),
+        ],
       ),
     );
   }
 }
-
-
-
-
